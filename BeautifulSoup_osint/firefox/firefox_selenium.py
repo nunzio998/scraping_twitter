77264ffa7d0254e70c88c1628172fbc87782c984
francesco <francesco.pinsone@alcampus.it>
2024-05-24 10:59:08 +0200

    pulizia codice & prima analisi html

--- BeautifulSoup_osint/firefox/firefox_selenium.py
+++ BeautifulSoup_osint/firefox/firefox_selenium.py
@@ -1,40 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
-import json
 from bs4 import BeautifulSoup
-from selenium.webdriver import FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-
-
-fp = webdriver.FirefoxProfile()
-
-
-fp.set_preference("browser.download.folderList",2)
-
-fp.set_preference("javascript.enabled", True)
 
 # Geckodriver
 service = Service('driver/geckodriver')
 
 # Inizializzo driver  Firefox
-options = webdriver.FirefoxOptions()
-
-# Opzioni driver (su browser con about:support)
-options.add_argument('--user-data-dir=/Users/francesco/Library/Application Support/Firefox/Profiles/i5qodkh1.default-release-1716024453688')# Questo utilizza il tuo profilo Firefox
-options.add_argument("javascript.enabled")
-
-driver = webdriver.Firefox(service=service, options=options)
-#driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver = webdriver.Firefox(service=service)
+# driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 # Loggarsi manualmente su Twitter
 driver.get('https://www.twitter.com/login')
 input("Premi Enter dopo aver effettuato il login...")
 
 # Mando richiesta get con query nei parametri
-#search_url = 'https://x.com/search?q=killnet&src=typed_query'
+# search_url = 'https://x.com/search?q=killnet&src=typed_query'
 search_url = "https://x.com/search?f=top&q=Killnet%20lang%3Aen%20-filter%3Alinks%20-filter%3Areplies&src=typed_query"
 driver.get(search_url)
 
@@ -48,5 +29,17 @@ soup = BeautifulSoup(html_content, 'html.parser')
 # Salvo la risposta in un file HTML
 with open('data_results/response.html', 'w') as f:
     f.write(soup.prettify())
+    print(soup.prettify())
+
+# Mi sposto sul body lasciando stare il resto dell'html
+soup = soup.body
+
+# results = soup.find_all("div", class_="css-175oi2r")
+#results = soup.find_all("div", string="killnet")
+results = soup.find_all("span", class_="css-1jxf684")
+# salvo i risultati in un file
+with open('data_results/results.html', 'w') as f:
+    f.write(str(results))
+    print("dati salvati")
 
 driver.quit()
