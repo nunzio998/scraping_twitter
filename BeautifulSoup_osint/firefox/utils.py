beb368268795f1a3c525a4aafe6edd1852079670
francesco <francesco.pinsone@alcampus.it>
2024-06-07 17:25:38 +0200

    aggiunta funzioni mongodb & cambiamenti nella logico

--- BeautifulSoup_osint/firefox/utils.py
+++ BeautifulSoup_osint/firefox/utils.py
@@ -1,11 +1,58 @@
 import json
 import re
 from datetime import datetime
+import pymongo
 
+interest_groups = ["Killnet", "NoName057(16)", "Lazarus", "DarkHalo", "MustangPanda", "BlackEnergy", "BadMagic",
+                   "DarkSide", "LockBit", "DopplePaymer", "RagnarLocker",
+                   "REvil", "DragonFly"]
+
+
+def read_json(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+
+
+config_data = read_json('mongo_utils.json')
+
+
+# Funzioni MongoDB:
+def connect_to_mongo():
+    connection_string = config_data['connection_string']
+    client = pymongo.MongoClient(connection_string)
+    # Provo a connettermi al database
+    try:
+        client.admin.command('ping')
+        print("Connesso al database: ", client.server_info()["version"])
+    except Exception as e:
+        print(e)
+
+    return client
+
+
+def connect_to_mongo_collection(client, collection_name):
+    db = client.get_database(config_data['database'])
+
+    # Verifica se la collezione esiste già
+    if collection_name not in db.list_collection_names():
+        # Se la collezione non esiste, creala
+        db.create_collection(collection_name)
+        print("Creata la collezione:", collection_name)
+    else:
+        print("La collezione esiste già:", collection_name)
+
+    return db.get_collection(collection_name)
+
+
+def save_to_mongo(data, collection):
+    collection.insert_one(data)
+    print("Salvato nel database: ", data['url'])
+
+
+# config_data = read_json('mongo_utils.json')
 
 # Leggo il file e divido i post
 def read_posts(filtered_lines):
-
     posts = []  # lista che conterrà tutti i post
     post = []  # lista di appoggio temporanea per salvare le righe di un post
 
@@ -13,6 +60,7 @@ def read_posts(filtered_lines):
         # controllo se lines[i] è l'ultimo elemento della lista
         stripped_line = filtered_lines[i].strip()
 
+        # se sono all'ultimo elemento della lista, aggiungo la riga a post e poi aggiungo post a posts
         if i == len(filtered_lines) - 1:
             post.append(stripped_line)
             posts.append(post)
@@ -25,7 +73,6 @@ def read_posts(filtered_lines):
         else:  # ho letto un intero post
             posts.append(post)
             post = [stripped_line]  # resetto la lista di appoggio per il prossimo post
-    print(posts)
     return posts
 
 
@@ -43,7 +90,8 @@ def parse_post(lines):
     # So che il contenuto del post si trova dalla quarta riga in poi. Utilizzando un'espressione regolare, posso ignorare le righe che contengono solo numeri.
     # Ovvero quelle righe che contengono solo il numero di like, commenti, condivisioni, ecc.
     contenuto_lines = lines[4:]
-    contenuto = ' '.join(line.strip() for line in contenuto_lines if not re.match(r'^\d+(\.\d+)?$', line.strip()) and not re.match(r'^https://', line.strip()))
+    contenuto = ' '.join(line.strip() for line in contenuto_lines if
+                         not re.match(r'^\d+(\.\d+)?$', line.strip()) and not re.match(r'^https://', line.strip()))
 
     # Considerando tutti gli elementi di lines prendo solo quelli che contengono solo numeri e li inserisco in una lista
     numeri = [line.strip() for line in lines if re.match(r'^\d+(\.\d+)?$', line.strip())]
@@ -73,9 +121,11 @@ def parse_post(lines):
         like = None
         visualizzazioni = None
 
-
-    # L'ultimo elemento sarà l'url del post
-    url = lines[-1].strip()
+    # L'ultimo elemento sarà l'url del post, altrimenti scarto il post.
+    if lines[-1].strip().startswith('https://'):
+        url = lines[-1].strip()
+    else:
+        return None
 
     # Ritorno il post in formato json
     return {
@@ -91,17 +141,19 @@ def parse_post(lines):
     }
 
 
-def read_parse_save(filtered_lines, file_path_out):
-    posts_lines = read_posts(filtered_lines)
+def read_parse_save(posts_to_save, group_name, client):
+    posts_lines = read_posts(posts_to_save)
 
-    parsed_posts = []  # lista che conterrà tutti i post parsati in formato json
+    # Connessione alla collezione dati
+    collection = connect_to_mongo_collection(client, group_name)
 
-    # Parso ogni post e lo aggiungo alla lista parsed_posts
+    # Parso ogni post e lo aggiungo al database
     for post_lines in posts_lines:
         parsed_post = parse_post(post_lines)
-        if parsed_post:
-            parsed_posts.append(parsed_post)
 
-    # salvo la lista di post parsati in un file json
-    with open(file_path_out, 'w', encoding='utf-8') as json_file:
-        json.dump(parsed_posts, json_file, ensure_ascii=False, indent=4)
+        # Salvo il post parsato nel database
+        if parsed_post:
+            save_to_mongo(parsed_post, collection)
+
+
+
