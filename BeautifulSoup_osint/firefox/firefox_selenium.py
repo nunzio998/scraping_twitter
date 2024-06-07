87237ca97f7e0cd8ff7520995af234eb4d2c0b36
francesco <francesco.pinsone@alcampus.it>
2024-06-07 10:02:36 +0200

    aggiunta loop for

--- BeautifulSoup_osint/firefox/firefox_selenium.py
+++ BeautifulSoup_osint/firefox/firefox_selenium.py
@@ -3,6 +3,10 @@ from selenium.webdriver.firefox.service import Service
 import time
 from bs4 import BeautifulSoup
 
+interest_groups = ["Killnet", "NoName057(16)", "Lazarus", "DarkHalo", "MustangPanda", "BlackEnergy", "BadMagic",
+                   "DarkSide", "LockBit", "DopplePaymer", "RagnarLocker",
+                   "REvil", "DragonFly"]
+
 # Geckodriver
 service = Service('driver/geckodriver')
 
@@ -14,20 +18,20 @@ driver = webdriver.Firefox(service=service)
 driver.get('https://www.twitter.com/login')
 input("Premi Enter dopo aver effettuato il login...")
 
-# Mando richiesta get con query nei parametri
-# search_url = 'https://x.com/search?q=killnet&src=typed_query'
-search_url = "https://x.com/search?f=top&q=killnet%20lang%3Aen%20-filter%3Alinks%20-filter%3Areplies&src=typed_query"
-driver.get(search_url)
+for group in interest_groups:
+    # Mando richiesta get con query nei parametri
+    search_url = f"https://x.com/search?f=top&q={group}%20lang%3Aen%20-filter%3Alinks%20-filter%3Areplies&src=typed_query"
+    driver.get(search_url)
 
-# Attendo caricamento pagina
-time.sleep(5)
+    # Attendo caricamento pagina
+    time.sleep(5)
 
-# Estraggo HTML pagina con BeautifulSoup e lo stampo
-html_content = driver.page_source
-soup = BeautifulSoup(html_content, 'html.parser')
+    # Estraggo HTML pagina con BeautifulSoup e lo stampo
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-# Salvo la risposta in un file HTML
-with open('data_results/response.html', 'w') as f:
-    f.write(soup.prettify())
+    # Salvo la risposta in un file HTML
+    with open(f'data_results/{group}.html', 'w') as f:
+        f.write(soup.prettify())
 
 driver.quit()
