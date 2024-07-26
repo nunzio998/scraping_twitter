70433abe02ac9adf38a875ae9a1859fcfa53fab7
francesco <francesco.pinsone@alcampus.it>
2024-07-26 12:24:26 +0200

    ahmia scraper draft

---
+++ DarkWeb_scraping/ahmia_browser_scraper.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+proxies = {
+    'http': 'socks5h://127.0.0.1:9050',
+    'https': 'socks5h://127.0.0.1:9050'
+}
+
+session = requests.Session()
+session.proxies.update(proxies)
+
+
+# Funzione per cercare in Ahmia
+def search_ahmia(query):
+    """
+    Funzione per cercare in Ahmia, uno dei motori di ricerca per il dark web.
+    :param query:
+    :return:
+    """
+    # URL di Ahmia versione onion
+    url = f'http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/search/?q={query}'
+
+    try:
+        response = session.get(url)
+        response.raise_for_status()  # Verifica se la richiesta è andata a buon fine
+    except requests.exceptions.RequestException as e:
+        print(f"Errore di connessione: {e}")
+        return []
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    soup = soup.body
+
+    results = []
+    for result in soup.find_all('li', class_='result'): # scorre la lista degli elementi che risultano dalla ricerca.
+        title = result.find('a').text
+        link = result.find('a')['href']
+        snippet = result.find('p').text
+        results.append({'title': title, 'link': link, 'snippet': snippet})
+
+    return results
+
+
+# Esempio di ricerca
+query = 'hacker attack energy infrastructure'
+results = search_ahmia(query)
+
+# Stampa dei risultati
+for result in results:
+    print(f"Title: {result['title']}")
+    print(f"Link: {result['link']}")
+    print(f"Snippet: {result['snippet']}\n")
+
+try:
+    response = session.get('http://example.onion')
+    print(response.text)
+except requests.exceptions.RequestException as e:
+    print(f"Errore di connessione al sito onion: {e}")
