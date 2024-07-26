f727354affc8062e91e7b90ab0494893bcfdc433
francesco <francesco.pinsone@alcampus.it>
2024-07-26 13:33:41 +0200

    mongodb functions

---
+++ DarkWeb_scraping/utils.py
@@ -0,0 +1,53 @@
+import json
+import re
+import time
+
+import pymongo
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def read_json(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+
+
+config_data = read_json('utils/credentials.json')
+
+
+# Funzioni MongoDB:
+def connect_to_mongo():
+    connection_string = config_data['connection_string']
+    client = pymongo.MongoClient(connection_string)
+    # Provo a connettermi al database
+    try:
+        client.admin.command('ping')
+        print("Connesso al database: ", client.server_info()["version"])
+    except Exception as e:
+        print(e)
+
+    return client
+
+
+def disconnect_to_mongo(client):
+    print("Disconnesso dal database: ", client.server_info()["version"])
+    client.close()
+
+
+def connect_to_mongo_collection(client, collection_name):
+    db = client.get_database(config_data['database'])
+
+    # Verifica se la collezione esiste già
+    if collection_name not in db.list_collection_names():
+        # Se la collezione non esiste, creala
+        db.create_collection(collection_name)
+        print("Creata la collezione:", collection_name)
+    else:
+        print("La collezione esiste già:", collection_name)
+
+    return db.get_collection(collection_name)
+
+
+def save_to_mongo(data, collection):
+    collection.insert_one(data)
